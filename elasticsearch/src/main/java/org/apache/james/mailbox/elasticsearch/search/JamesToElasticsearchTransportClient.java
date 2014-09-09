73621549c8710f3685b61822dd2b42aa445b61db
Philippe BENOIT <pbenoit@linagora.com>
2014-09-09 15:26:02 +0200

    Create a TransportClient for James

---
+++ elasticsearch/src/main/java/org/apache/james/mailbox/elasticsearch/search/JamesToElasticsearchTransportClient.java
@@ -0,0 +1,25 @@
+package org.apache.james.mailbox.elasticsearch.search;
+
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
+
+public class JamesToElasticsearchTransportClient extends TransportClient {
+
+    private static Settings settings = ImmutableSettings.settingsBuilder()
+            .put("cluster.name", "james").build();
+
+    public JamesToElasticsearchTransportClient(String hostname, int  port) throws ElasticsearchException {
+        super(settings);
+        addTransportAddress(new InetSocketTransportAddress(hostname, port));
+    }
+    
+   public JamesToElasticsearchTransportClient(TransportAddress[] transportAddress) {
+       super(settings);
+       addTransportAddresses(transportAddress);
+   }
+
+}
